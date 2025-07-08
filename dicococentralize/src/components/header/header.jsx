f170b7be20a53bdda81f41bbad8e376fc4ebc6a1
MattSouza14 <mateussouzabr1@gmail.com>
2025-07-08 16:02:37 -0300

    Commit inicial

---
+++ dicococentralize/src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+const Header = () => {
+  return (
+    <header>
+      <h1>Dicoco Centralize</h1>
+    </header>
+  );
+};
+
+export default Header;
